692d0b84ebc835d470a7b2cb982aa09a4dee30e6
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-23 10:42:20 +0200

    adding style to navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,16 @@
 import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
 import Head from "next/head";
 import React from "react";
+import useStyles from "../utils/styles";
 
 const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Madotech</title>
       </Head>
-      <AppBar position="static">
+      <AppBar className={classes.navbar} position="static">
         <Toolbar>
           <Typography>Madotech</Typography>
         </Toolbar>
