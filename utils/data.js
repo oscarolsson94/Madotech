653ca9746f27a0e0422ef1d6d070baec38cf0468
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-23 11:30:58 +0200

    adding drums to products

---
+++ utils/data.js
@@ -0,0 +1,29 @@
+const data = {
+  products: [
+    {
+      name: "1.5 liter drum",
+      category: "Drums",
+      image: "/images/101001.jpg",
+    },
+    {
+      name: "2.0 liter drum",
+      category: "Drums",
+      image: "/images/101002.jpg",
+    },
+    {
+      name: "3.0 liter drum",
+      category: "Drums",
+      image: "/images/101003.jpg",
+    },
+    {
+      name: "5.0 liter drum",
+      category: "Drums",
+      image: "/images/101005.jpg",
+    },
+    {
+      name: "10.0 liter drum",
+      category: "Drums",
+      image: "/images/101010.jpg",
+    },
+  ],
+};
