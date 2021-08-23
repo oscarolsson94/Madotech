27a2a78a53ef63988765bcb2e1bcc88b978b3b3e
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-23 15:58:29 +0200

    dynamic head tag

--- components/Layout.js
+++ components/Layout.js
@@ -10,12 +10,12 @@ import React from "react";
 import useStyles from "../utils/styles";
 import NextLink from "next/link";
 
-const Layout = ({ children }) => {
+const Layout = ({ title, children }) => {
   const classes = useStyles();
   return (
     <div>
       <Head>
-        <title>Madotech</title>
+        <title>{title ? `${title} - Madotech` : "Madotech"} </title>
       </Head>
       <AppBar className={classes.navbar} position="static">
         <Toolbar>
