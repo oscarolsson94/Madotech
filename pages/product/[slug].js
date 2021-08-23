6267baabdc33bc654659fd02a687e1a924dbaee1
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-23 16:19:46 +0200

    showing product image dynamically

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,6 +4,7 @@ import React from "react";
 import Layout from "../../components/Layout";
 import data from "../../utils/data";
 import NextLink from "next/link";
+import Image from "next/image";
 import useStyles from "../../utils/styles";
 
 const ProductScreen = () => {
@@ -26,7 +27,15 @@ const ProductScreen = () => {
         </NextLink>
       </div>
       <Grid container spacing={1}>
-        <Grid item md={6} xs={12}></Grid>
+        <Grid item md={6} xs={12}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            layout="responsive"
+          ></Image>
+        </Grid>
       </Grid>
     </Layout>
   );
