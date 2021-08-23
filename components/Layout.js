edf37582370a410523a0771c89da250cd5ed5d4c
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-23 10:36:27 +0200

    adding basic layout for front page

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import Head from "next/head";
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Head>
+        <title>Madotech</title>
+      </Head>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography>Madotech</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>{children}</Container>
+      <footer>
+        <Typography>All rights reserved. Madotech</Typography>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
