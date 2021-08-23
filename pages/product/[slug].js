e8a961726d84457ed4e0e0e116764fe543e38215
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-23 15:49:26 +0200

    creating dynamic page & accessing the prod slug

---
+++ pages/product/[slug].js
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router";
+import React from "react";
+import data from "../../utils/data";
+
+const ProductScreen = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const product = data.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    return <div>Product Not Found</div>;
+  }
+
+  return (
+    <div>
+      <h1>{product.name}</h1>
+    </div>
+  );
+};
+
+export default ProductScreen;
