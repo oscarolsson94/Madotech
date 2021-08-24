e96fcd44f5ad49d605d91e4fa566d95fa6b3e29e
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-24 19:17:50 +0200

    fix hoses and create pumps page

--- pages/hoses.js
+++ pages/hoses.js
@@ -13,38 +13,37 @@ import data from "../utils/data";
 import NextLink from "next/link";
 
 export default function Hoses() {
-  const products = data.products.filter(
-    (product) => product.category === "Hoses"
-  );
   return (
     <Layout>
       <div>
         <h1>Hoses</h1>
         <Grid container spacing={3}>
-          {products.map((product) => (
-            <Grid item md={3} sm={6} xs={12} key={product.image}>
-              <Card>
-                <NextLink href={`/product/${product.slug}`} passHref>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      image={product.image}
-                      title={product.name}
-                    ></CardMedia>
-                    <CardContent>
-                      <Typography>{product.name}</Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </NextLink>
-                <CardActions>
-                  <Typography>{product.price} kr</Typography>
-                  <Button size="small" variant="contained" color="primary">
-                    Add to cart
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
+          {data.products
+            .filter((product) => product.category === "Hoses")
+            .map((product) => (
+              <Grid item md={3} sm={6} xs={12} key={product.image}>
+                <Card>
+                  <NextLink href={`/product/${product.slug}`} passHref>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        image={product.image}
+                        title={product.name}
+                      ></CardMedia>
+                      <CardContent>
+                        <Typography>{product.name}</Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </NextLink>
+                  <CardActions>
+                    <Typography>{product.price} kr</Typography>
+                    <Button size="small" variant="contained" color="primary">
+                      Add to cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
         </Grid>
       </div>
     </Layout>
---
+++ pages/pumps.js
@@ -0,0 +1,80 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import Layout from "../components/Layout";
+import data from "../utils/data";
+import NextLink from "next/link";
+
+export default function Hoses() {
+  return (
+    <Layout>
+      <div>
+        <h1>Standard pumps</h1>
+        <Grid container spacing={3}>
+          {data.products
+            .filter((product) => product.category === "StandardPumps")
+            .map((product) => (
+              <Grid item md={3} sm={6} xs={12} key={product.image}>
+                <Card>
+                  <NextLink href={`/product/${product.slug}`} passHref>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        image={product.image}
+                        title={product.name}
+                      ></CardMedia>
+                      <CardContent>
+                        <Typography>{product.name}</Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </NextLink>
+                  <CardActions>
+                    <Typography>{product.price} kr</Typography>
+                    <Button size="small" variant="contained" color="primary">
+                      Add to cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
+        <h1>Premium pumps</h1>
+        <Grid container spacing={3}>
+          {data.products
+            .filter((product) => product.category === "PremiumPumps")
+            .map((product) => (
+              <Grid item md={3} sm={6} xs={12} key={product.image}>
+                <Card>
+                  <NextLink href={`/product/${product.slug}`} passHref>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        image={product.image}
+                        title={product.name}
+                      ></CardMedia>
+                      <CardContent>
+                        <Typography>{product.name}</Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </NextLink>
+                  <CardActions>
+                    <Typography>{product.price} kr</Typography>
+                    <Button size="small" variant="contained" color="primary">
+                      Add to cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
+      </div>
+    </Layout>
+  );
+}
