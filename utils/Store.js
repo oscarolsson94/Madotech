2c4f8785144e1b1c811930f899af3fcc914ed28d
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-24 10:33:04 +0200

    returning Store.Provider to wrap the app

--- utils/Store.js
+++ utils/Store.js
@@ -20,4 +20,5 @@ function reducer(state, action) {
 export function StoreProvider(props) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const value = { state, dispatch };
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
 }
