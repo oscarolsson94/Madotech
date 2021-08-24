95f8a90151e82d05b478bf4433546a54eb98d3bb
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-24 10:31:22 +0200

    setting up reducer for theme

--- utils/Store.js
+++ utils/Store.js
@@ -6,6 +6,18 @@ const initialState = {
   darkMode: false,
 };
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "DARK_MODE_ON":
+      return { ...state, darkMode: true };
+    case "DARK_MODE_OFF":
+      return { ...state, darkMode: false };
+    default:
+      return state;
+  }
+}
+
 export function StoreProvider(props) {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const value = { state, dispatch };
 }
