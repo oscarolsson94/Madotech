2d2a683eec72bf65263c66e931c622e5c288f7ea
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-25 09:19:57 +0200

    making connect util function

--- utils/db.js
+++ utils/db.js
@@ -7,4 +7,19 @@ async function connect() {
     console.log("already connected");
     return;
   }
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+    if (connection.isConnected === 1) {
+      console.log("use previous connection");
+      return;
+    }
+    await mongoose.disconnect();
+  }
+  const db = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+  console.log("new connection");
+  connection.isConnected = db.connections[0].readyState;
 }
