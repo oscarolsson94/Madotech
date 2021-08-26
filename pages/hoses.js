45941f0f4585616871a2682baf687636d0fff8f9
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-26 09:18:47 +0200

    fixing bug with existing products in cart

--- pages/hoses.js
+++ pages/hoses.js
@@ -19,9 +19,11 @@ import { Store } from "../utils/Store";
 
 function Hoses() {
   const classes = useStyles();
-  const { dispatch } = useContext(Store);
+  const { state, dispatch } = useContext(Store);
   const addToCartHandler = (product) => {
-    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity: 1 } });
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
   };
   return (
     <Layout>
