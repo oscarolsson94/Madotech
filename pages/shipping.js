fdb4c58e1cbc5a0802a6f6bcfdc455dab0c7c362
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-08-27 14:26:18 +0200

    setting up state and constants

--- pages/shipping.js
+++ pages/shipping.js
@@ -5,7 +5,30 @@ import {
   TextField,
   Button,
 } from "@material-ui/core";
+import { useRouter } from "next/router";
+import React, { useContext, useEffect } from "react";
+import Layout from "../components/Layout";
+import { Store } from "../utils/Store";
+import useStyles from "../utils/styles";
+import Cookies from "js-cookie";
+import { Controller, useForm } from "react-hook-form";
+import CheckoutWizard from "../components/CheckoutWizard";
 
+export default function Shipping() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+    getValues,
+  } = useForm();
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  const {
+    cart: { shippingAddress },
+  } = state;
+
+  
   };
 
   return (
